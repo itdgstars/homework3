4ed8306ebce0ec68dc1936a59f62dd9c3def30b3
SergeyK <it-dgstars@mail.ru>
2023-02-11 16:08:27 +0300

    решили задачу 23

---
+++ z23/Program.cs
@@ -0,0 +1,19 @@
+﻿void Quards(int uN)
+{
+    int current = 1;
+    while (current <= uN)
+    {
+        int res=current*current*current;
+        Console.Write($"{res}, ");
+        current++;
+    }
+    Console.WriteLine("\b\b.");
+}
+Console.Write("введите число N ");
+int uNN = Convert.ToInt32(Console.ReadLine());
+if (uNN >=1)
+{
+    Console.Write($"{uNN} -> ");
+   Quards(uNN); 
+}
+else Console.WriteLine("Неправильное число");
